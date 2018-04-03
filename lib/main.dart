3caad5c325662bab80a410cc57e82a2015738569
Simone Sestito <simone.dev.help@gmail.com>
2018-04-03 16:05:28 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'parser.dart';
+
+void main() => runApp(new MyApp());
+
+const appName = 'Flutter Calculator';
+
+var digits = <String>['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var operators = <String>['+', '-', '*', '/'];
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: appName,
+      theme: new ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.blueAccent,
+        accentColor: Colors.cyanAccent,
+        buttonColor: Colors.grey,
+      ),
+      home: new Main(),
+    );
+  }
+}
+
+class Main extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.white,
+        title: new Text(
+          appName,
+          style: new TextStyle(
+            color: Theme.of(context).primaryColor,
+          ),
+        ),
+      ),
+      body: new Column(
+        children: <Widget>[
+          new Display(),
+          new Keyboard(),
+        ],
+      ),
+    );
+  }
+}
+
+var _displayState = new DisplayState();
+
+class Display extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _displayState;
+  }
+}
+
+class DisplayState extends State<Display> {
+  var _expression = '';
+  var _resultDisplayed = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Expanded(
+      flex: 1,
+      child: new Center(
+        child: new Text(
+          _expression,
+          textAlign: TextAlign.end,
+          style: new TextStyle(
+            fontSize: 40.0,
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+bool _addKey(String key) {
+  var _expr = _displayState._expression;
+  var _result = false;
+  if (_displayState._resultDisplayed) _expr = '';
+
+  if (operators.contains(key)) {
+    // Handle as an operator
+    if (_expr.length > 0 && operators.contains(_expr[_expr.length - 1])) {
+      _expr = _expr.substring(0, _expr.length - 1);
+    }
+    _expr += key;
+  } else if (digits.contains(key)) {
+    // Handle as an operand
+    _expr += key;
+  } else if (key == 'C') {
+    // Delete last character
+    if (_expr.length > 0) {
+      _expr = _expr.substring(0, _expr.length - 1);
+    }
+  } else if (key == '=') {
+    try {
+      var parser = const Parser();
+      _expr = parser.parseExpression(_expr).toString();
+      _result = true;
+    } on Error {
+      print('ERROR');
+      return false;
+    }
+  }
+  _displayState.setState(() {
+    _displayState._expression = _expr;
+    _displayState._resultDisplayed = _result;
+  });
+  return true;
+}
+
+class Keyboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Expanded(
+        flex: 3,
+        child: new GridView.count(
+          crossAxisCount: 4,
+          childAspectRatio: 1.0,
+          padding: const EdgeInsets.all(4.0),
+          mainAxisSpacing: 4.0,
+          crossAxisSpacing: 4.0,
+          children: <String>[
+            '7', '8', '9', '/',
+            '4', '5', '6', '*',
+            '1', '2', '3', '-',
+            'C', '0', '=', '+',
+          ].map((key) {
+            return new GridTile(
+              child: new KeyboardKey(key),
+            );
+          }).toList(),
+        ));
+  }
+}
+
+class KeyboardKey extends StatelessWidget {
+  KeyboardKey(this._keyValue);
+
+  final _keyValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return new FlatButton(
+      child: new Text(
+        _keyValue,
+        style: new TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 26.0,
+          color: Colors.black,
+        ),
+      ),
+      color: Theme.of(context).scaffoldBackgroundColor,
+      onPressed: () {
+        var success = _addKey(_keyValue);
+        if (!success) {
+          Scaffold.of(context).showSnackBar(new SnackBar(
+                content: new Text('Error'),
+              ));
+        }
+      },
+    );
+  }
+}
---
+++ lib/parser.dart
@@ -0,0 +1,111 @@
+import 'dart:math';
+import 'dart:collection';
+
+class Parser {
+  const Parser();
+
+  num _eval(num op1, num op2, int op) {
+    switch (new String.fromCharCode(op)) {
+      case '+':
+        return op1 + op2;
+      case '-':
+        return op1 - op2;
+      case '*':
+        return op1 * op2;
+      case '/':
+        return op1 / op2;
+      case '%':
+        return op1 % op2;
+      case '^':
+        return pow(op1, op2);
+      default:
+        return 0;
+    }
+  }
+
+  int _getPriority(num op) {
+    switch (new String.fromCharCode(op)) {
+      case '+':
+      case '-':
+        return 0;
+      case '*':
+      case '/':
+      case '%':
+        return 1;
+      case '^':
+        return 2;
+      default:
+        return -1;
+    }
+  }
+
+  bool _isOperator(num op) {
+    switch (new String.fromCharCode(op)) {
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+      case '^':
+      case '%':
+        return true;
+      default:
+        return false;
+    }
+  }
+
+  bool _isDigit(int op) {
+    return op >= '0'.codeUnitAt(0) && op <= '9'.codeUnitAt(0);
+  }
+
+  num parseExpression(String expr) {
+    Queue<int> operators = new ListQueue<int>();
+    Queue<num> operands = new ListQueue<num>();
+
+    // True if the last character was a digit
+    // to accept numbers with more digits
+    bool lastDig = true;
+
+    // INIT
+    operands.addLast(0);
+
+    expr.runes.forEach((int c) {
+      if (_isDigit(c)) {
+        if (lastDig) {
+          num last = operands.removeLast();
+          operands.addLast(last * 10 + (c - '0'.codeUnitAt(0)));
+        } else
+          operands.addLast(c - '0'.codeUnitAt(0));
+      } else if (_isOperator(c)) {
+        if (!lastDig) throw new ArgumentError('Illegal expression');
+
+        if (operators.isEmpty)
+          operators.addLast(c);
+        else {
+          while (operators.isNotEmpty &&
+              operands.isNotEmpty &&
+              _getPriority(c) <= _getPriority(operators.last)) {
+            num op1 = operands.removeLast();
+            num op2 = operands.removeLast();
+            int op = operators.removeLast();
+
+            num res = _eval(op2, op1, op);
+            operands.addLast(res);
+          }
+          operators.addLast(c);
+        }
+      }
+      lastDig = _isDigit(c);
+    });
+
+    while (operators.isNotEmpty) {
+      num op1 = operands.removeLast();
+      num op2 = operands.removeLast();
+      int op = operators.removeLast();
+
+      num res = _eval(op2, op1, op);
+      operands.addLast(res);
+    }
+
+    return operands.removeLast();
+  }
+}
